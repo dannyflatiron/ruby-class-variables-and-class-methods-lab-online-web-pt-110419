506cb4910b4f2da97a57efb7d0e66ed22c8245bd
Daniel Reina <dannyflatiron@gmail.com>
2019-12-03 05:03:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,18 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@genre = []
 
   def initialize(name, artist, genre)
     @@count += 1
+    @@genre << genre
   end
 
   def self.count
     @@count
   end
 
+  def self.genres
+    @genres.uniq!
+  end
 end
